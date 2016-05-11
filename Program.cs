b3fc875b563ee60542103f78447a34c22d85aa6b
elitsarangelova <elitsa.rangelova95@gmail.com>
2016-05-11 11:42:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace area
+{
+    class Program
+    {
+        static void Main()
+        {
+            var a = decimal.Parse(Console.ReadLine());
+            var b = decimal.Parse(Console.ReadLine());
+            Console.WriteLine(a * b);
+        }
+    }
+}
